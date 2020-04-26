6a53e1b9ec870bd6f6969f65b5b8540c3e3fa06f
lakkot <40659615+lakkot@users.noreply.github.com>
2020-04-26 17:20:59 +0200

    styling frontend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,12 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+
+
 
 
 import { AppComponent } from './app.component';
@@ -27,8 +33,12 @@ const appRoutes: Routes = [
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     HttpClientModule,
     ReactiveFormsModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatButtonModule,
     RouterModule.forRoot(appRoutes)
   ],
   providers: [],
